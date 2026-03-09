44c5c419be8ab5d306683a8b93a30ac7f5bb9b97
Afridul <afridul.dev@gmail.com>
2026-03-09 06:14:21 +0600

    sort by  done

--- src/Pages/ReadList/ReadList.jsx
+++ src/Pages/ReadList/ReadList.jsx
@@ -2,13 +2,14 @@ import { useEffect, useState } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import { getStoredData } from '../../Utilities/localStorageDB';
-import { useLoaderData } from 'react-router';
+import { useAsyncError, useLoaderData } from 'react-router';
 import Book from '../../Components/Book/Book';
 
 
 const ReadList = () => {
     const data = useLoaderData()
     const [readList,setReadList] = useState([])
+    const [sort,setSort] = useState('')
     
     useEffect(()=>{
         const storedReadList = getStoredData()
@@ -16,9 +17,28 @@ const ReadList = () => {
         setReadList(readListBooks)
     },[])
 
+    const handleSort=(types)=>{
+        setSort(types)
+        if (types === 'Pages') {
+            const sorted = [...readList].sort((a,b)=> a.totalPages-b.totalPages)
+            setReadList(sorted)
+        }
+        if (types === 'Ratings') {
+            const sorted = [...readList].sort((a,b)=> a.rating-b.rating)
+            setReadList(sorted)
+        }
+    }
+
   return (
     <div className='pb-28 pt-6'>
         <h1 className='text-4xl font-bold text-center mt-10'>Books</h1>
+        <div className="dropdown mx-auto flex justify-center my-6">
+            <div tabIndex={0} role="button" className="btn m-1">Sort By : {sort?sort:''}</div>
+            <ul tabIndex="-1" className="dropdown-content menu bg-base-100 rounded-box z-1 mt-14 w-52 p-2 shadow-sm">
+                <button onClick={()=> handleSort('Pages')} className='btn btn-outline'>Pages</button>
+                <button onClick={()=> handleSort('Ratings')} className='btn btn-outline'>Ratings</button>
+            </ul>
+        </div>
         <Tabs>
             <TabList>
             <Tab>My Read List</Tab>
